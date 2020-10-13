db2a315d1355d7e5740a5dc774797dbc61e583d9
kitpao <kitpao91@gmail.com>
2020-10-13 19:37:03 +0000

    prompt user for the name and personalize welcome message

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,28 @@ myImage.onclick = function() {
     myImage.setAttribute('src', 'images/firefox-icon.png');
   }
 }
+
+// change the page title to a personalized welcome message when the user first visits the site.
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) { //check for blank or null values
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+// add the possibility to change name
+myButton.onclick = function() {
+  setUserName();
+}
