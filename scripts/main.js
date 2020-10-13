986d1493e66d7dc4218482e2fadbd37cbda3c8ce
kitpao <kitpao91@gmail.com>
2020-10-13 19:18:26 +0000

    Add an image for toggling on click and its code

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,18 @@
+// change title
+/*
 const myHeading = document.querySelector('h1');
 myHeading.textContent = "Hello world!";
+*/
+
+//toggle image on click
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute('src', 'images/firefox2.png');
+  } else {
+    myImage.setAttribute('src', 'images/firefox-icon.png');
+  }
+}
